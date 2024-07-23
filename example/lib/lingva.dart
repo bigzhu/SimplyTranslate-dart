f1488cdbc866e8d954faa211c0fcff68052deabe
bigzhu <vermiliondun@gmail.com>
2024-07-23 19:35:04 +0800

    update

--- example/lib/lingva.dart
+++ example/lib/lingva.dart
@@ -8,11 +8,11 @@ void main() async {
   final vt = SimplyTranslator(EngineType.google);
 
   ///using Lingva Translate long form, getting list of multiple translations (only for words)
-  var listResult = await vt.translateLingva(
+  await vt.translateLingva(
     "Great ideas aren't the product of sudden inspiration but rather a gradual connection of decades of concepts and progress. Bitcoin and the broader cryptocurrency industry are the end result of five decades of innovation",
     'en',
     'zh',
     InstanceMode.Loop,
   );
-  print(listResult);
+  // print(listResult);
 }
